d5ef90e929dac3ccd76b210f5544a3c7cd4c4877
Troy Frew <troy.frew@capitalone.com>
2014-10-21 13:58:30 -0400

    Updated mysql schema

--- src/de/bdh/ks/Database.java
+++ src/de/bdh/ks/Database.java
@@ -80,7 +80,7 @@ public class Database
         if(!rs.next())
         {
             System.out.println((new StringBuilder()).append("[KSALE] Creating table: ").append(configManager.SQLTable).append("_deliver").toString());
-            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_deliver(").append("`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`money` int(11) NOT NULL DEFAULT '0',`type` int(11) NOT NULL DEFAULT '0',`subtype` int(11) NOT NULL DEFAULT '0',`amount` int(11) NOT NULL DEFAULT '0',`player` varchar(50) NOT NULL,`zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`), KEY `player` (`player`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1").toString());
+            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_deliver(").append("`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`money` int(11) NOT NULL DEFAULT '0',`type` int(11) NOT NULL DEFAULT '0',`subtype` int(11) NOT NULL DEFAULT '0',`amount` int(11) NOT NULL DEFAULT '0',`player` varchar(64) NOT NULL,`zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`), KEY `player` (`player`)) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1").toString());
             ps.executeUpdate();
             System.out.println("[KSALE] Table deliver Created.");
         }
@@ -89,7 +89,7 @@ public class Database
         if(!rs.next())
         {
             System.out.println((new StringBuilder()).append("[KSALE] Creating table: ").append(configManager.SQLTable).append("_offer").toString());
-            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_offer(").append("`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`type` int(11) NOT NULL,`amount` int(11) NOT NULL,`subtype` int(11) NOT NULL,`price` int(11) NOT NULL,`player` varchar(50) NOT NULL, `admin` int(1) NOT NULL DEFAULT '0',`sworld` varchar(50) NOT NULL DEFAULT '',`signx` int(13) NOT NULL DEFAULT '0', `signy` int(13) NOT NULL DEFAULT '0', `signz` int(13) NOT NULL DEFAULT '0', `nodeliver` tinyint(4) NOT NULL DEFAULT '0', `zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`),KEY `sign` (`sworld`,`signx`,`signy`,`signz`), KEY `player` (`player`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1").toString());
+            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_offer(").append("`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`type` int(11) NOT NULL,`amount` int(11) NOT NULL,`subtype` int(11) NOT NULL,`price` int(11) NOT NULL,`player` varchar(64) NOT NULL, `admin` int(1) NOT NULL DEFAULT '0',`sworld` varchar(50) NOT NULL DEFAULT '',`signx` int(13) NOT NULL DEFAULT '0', `signy` int(13) NOT NULL DEFAULT '0', `signz` int(13) NOT NULL DEFAULT '0', `nodeliver` tinyint(4) NOT NULL DEFAULT '0', `zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`),KEY `sign` (`sworld`,`signx`,`signy`,`signz`), KEY `player` (`player`)) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1").toString());
             ps.executeUpdate();
             System.out.println("[KSALE] Table offer Created.");
         }
@@ -98,16 +98,16 @@ public class Database
         if(!rs.next())
         {
             System.out.println((new StringBuilder()).append("[KSALE] Creating table: ").append(configManager.SQLTable).append("_request").toString());
-            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_request(").append("`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`type` int(11) NOT NULL,`amount` int(11) NOT NULL,`subtype` int(11) NOT NULL,`price` int(11) NOT NULL,`player` varchar(50) NOT NULL, `admin` int(1) NOT NULL DEFAULT '0',`zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `sworld` varchar(50) NOT NULL DEFAULT '',`signx` int(13) NOT NULL DEFAULT '0', `signy` int(13) NOT NULL DEFAULT '0', `signz` int(13) NOT NULL DEFAULT '0', `nodeliver` tinyint(4) NOT NULL DEFAULT '0', PRIMARY KEY (`id`),KEY `sign` (`sworld`,`signx`,`signy`,`signz`), KEY `player` (`player`)) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1").toString());
+            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_request(").append("`id` int(11) unsigned NOT NULL AUTO_INCREMENT,`type` int(11) NOT NULL,`amount` int(11) NOT NULL,`subtype` int(11) NOT NULL,`price` int(11) NOT NULL,`player` varchar(64) NOT NULL, `admin` int(1) NOT NULL DEFAULT '0',`zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `sworld` varchar(50) NOT NULL DEFAULT '',`signx` int(13) NOT NULL DEFAULT '0', `signy` int(13) NOT NULL DEFAULT '0', `signz` int(13) NOT NULL DEFAULT '0', `nodeliver` tinyint(4) NOT NULL DEFAULT '0', PRIMARY KEY (`id`),KEY `sign` (`sworld`,`signx`,`signy`,`signz`), KEY `player` (`player`)) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1").toString());
             ps.executeUpdate();
-            System.out.println("[KSALE] Table offer Created.");
+            System.out.println("[KSALE] Table request Created.");
         }
         
         rs = dbm.getTables(null, null, (new StringBuilder()).append(configManager.SQLTable).append("_transaction").toString(), null);
         if(!rs.next())
         {
             System.out.println((new StringBuilder()).append("[KSALE] Creating table: ").append(configManager.SQLTable).append("_transaction").toString());
-            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_transaction(").append("`type` int(11) unsigned NOT NULL, `subtype` int(11) NOT NULL DEFAULT '0',`fromplayer` varchar(50) NOT NULL, `toplayer` varchar(50) NOT NULL, `amount` int(11) NOT NULL, `price` double unsigned NOT NULL,`zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, KEY  `type` ( `type` , `subtype` ), KEY `fromplayer` (`fromplayer`), KEY `toplayer` (`toplayer`), KEY `id` (`type`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;").toString());
+            ps = conn.prepareStatement((new StringBuilder()).append("CREATE TABLE ").append(configManager.SQLTable).append("_transaction(").append("`type` int(11) unsigned NOT NULL, `subtype` int(11) NOT NULL DEFAULT '0',`fromplayer` varchar(64) NOT NULL, `toplayer` varchar(64) NOT NULL, `amount` int(11) NOT NULL, `price` double unsigned NOT NULL,`zeit` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, KEY  `type` ( `type` , `subtype` ), KEY `fromplayer` (`fromplayer`), KEY `toplayer` (`toplayer`), KEY `id` (`type`)) ENGINE=MyISAM DEFAULT CHARSET=utf8;").toString());
             ps.executeUpdate();
             System.out.println("[KSALE] Table transaction Created.");
         }
